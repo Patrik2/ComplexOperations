5738c4ecfc891f031e189a4d7c281e546b08ed53
Patrik2 <platoriansk@gmail.com>
2016-05-17 12:02:59 +0200

    CommitComplexOperations

---
+++ ComplexNumber.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestComplex
+{
+    public struct ComplexNumber
+    {
+        public decimal ImagPart { get; set; }
+        public decimal RealPart { get; set; }
+        public ComplexNumber(decimal realPart,decimal imagPart)
+        {
+            ImagPart = imagPart;
+            RealPart = realPart;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} + {1}i", RealPart, ImagPart);
+        }
+        public static bool operator ==(ComplexNumber c1, ComplexNumber c2)
+        {
+            return c1.Equals(c2);
+        }
+        public static bool operator !=(ComplexNumber c1, ComplexNumber c2)
+        {
+            return !(c1.Equals(c2));
+        }
+        public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2) 
+        {
+            return (new ComplexNumber(c1.RealPart + c2.RealPart, c1.ImagPart + c2.ImagPart));
+        }
+
+        public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2) 
+        {
+            return (new ComplexNumber(c1.RealPart - c2.RealPart, c1.ImagPart - c2.ImagPart));
+        }
+        public static ComplexNumber operator *(ComplexNumber c1, ComplexNumber c2)  // operator *
+        {
+            
+            return (new ComplexNumber((c1.RealPart * c2.RealPart) - (c1.ImagPart * c2.ImagPart), (c1.ImagPart * c2.RealPart) + (c2.ImagPart * c1.RealPart)));
+        }
+        public static ComplexNumber operator /(ComplexNumber c1, ComplexNumber c2)
+        {
+            decimal denominator = (c2.RealPart * c2.RealPart) + (c2.ImagPart * c2.ImagPart);
+            var numerator = new ComplexNumber((c1.RealPart * c2.RealPart) + (c1.ImagPart * c2.ImagPart), (c1.ImagPart * c2.RealPart) - (c2.ImagPart * c1.RealPart));
+            return (new ComplexNumber(numerator.RealPart/denominator,numerator.ImagPart/denominator));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TestComplex
+{
+    // vytvoit plnohodnotnu strukturu na komplexne cisla a manipulaciu s nimi
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n1 = new ComplexNumber (4,2);
+            var n2 = new ComplexNumber (3,-1);
+            var n3 = new ComplexNumber (4,2); 
+            var n4 = n1;
+
+            Console.WriteLine("Result of addition:{0}", n1+n2+n3);
+            Console.WriteLine("Result of multiplication:{0}", n1 * n2);
+            Console.WriteLine("Result of substraction:{0}", n1 - n2);
+            Console.WriteLine("Result of division:{0}", n1 / n2);
+            Console.WriteLine("{0}", n1.Equals(n3));
+            Console.WriteLine("{0}", n1 == n3);
+            Console.WriteLine("{0}", n1 != n3);
+            Console.ReadLine();
+        }
+        //public struct ComplexNumber
+        //{
+        //    public int ImagPart { get; set; }
+        //    public int RealPart { get; set; }
+        //}
+
+        //public static ComplexNumber Addition(ComplexNumber k1, ComplexNumber k2) // operator +
+        //{
+        //    var result = new ComplexNumber();
+        //    result.RealPart = k1.RealPart + k2.RealPart;
+        //    result.ImagPart = k1.ImagPart + k2.ImagPart;
+        //    return result;
+        //}
+       
+        //public static ComplexNumber Substraction(ComplexNumber k1, ComplexNumber k2) // operator -
+        //{
+        //    var result = new ComplexNumber();
+        //    result.RealPart = k1.RealPart - k2.RealPart;
+        //    result.ImagPart = k1.ImagPart - k2.ImagPart;
+        //    return result;
+        //}
+        //public static ComplexNumber Multiplication(ComplexNumber k1, ComplexNumber k2)  // operator *
+        //{
+        //    var result = new ComplexNumber();
+        //    result.RealPart = (k1.RealPart * k2.RealPart) - (k1.ImagPart * k2.ImagPart);
+        //    result.ImagPart = ( k1.ImagPart * k2.RealPart) + (k1.ImagPart * k2.RealPart);
+        //    return result;
+        //}
+        //public static ComplexNumber Division(ComplexNumber k1, ComplexNumber k2)
+        //{
+        //    var result = new ComplexNumber();
+
+
+        //    result.RealPart = (k1.RealPart * k2.RealPart) - (k1.ImagPart * k2.ImagPart);
+        //    result.ImagPart = (k1.ImagPart * k2.RealPart) + (k1.ImagPart * k2.RealPart);
+        //    return result;
+        //}
+    }
+}
